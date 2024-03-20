3e1f3a9753ad6e53e6c08d9a9760f9e0acf9f07d
Duc Hop <DucLH.B21CN034@stu.ptit.edu.vn>
2024-03-20 16:35:49 +0700

    update files

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-// Data needed for first part of the section
 
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri','sat', 'sun'];
 const openingHours = {
@@ -56,6 +52,48 @@ const restaurant = {
 
 };
 
+/*
+*/
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+//solve
+
+const convert_inform = function(str) {
+  return str.replaceAll("_", " ").trim();
+}
+const convert_plot = function(str) {
+  return str.slice(0, 4).toUpperCase();
+}
+const convert_time = (str) => str.replace(":", "h");
+
+
+const display_informed_flight = function(){
+  const arr_message = flights.split('+');
+  // console.log(tmp);
+  let ans = [];
+  for(const message of arr_message) {
+    // convert each message to formal text
+
+    let [inform, start, end, time] = message.split(";");
+    ans.push([convert_inform(inform), convert_plot(start), convert_plot(end), convert_time(time)]);
+    
+  }
+  for (const e of ans) {
+    // let tmp = ""
+
+   const tmp =  `${e[0].includes("Delayed") ? "🔴 " : ""}${e[0]} from ${e[1]} to ${e[2]} (${e[3]})`;
+    console.log(tmp.padStart(50, " "));
+  }
+}
+
+display_informed_flight();
+
 /* 
 Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
 
@@ -83,20 +121,19 @@ HINT 4: This challenge is difficult on purpose, so start watching the solution i
 Afterwards, test with your own test data!
 
 GOOD LUCK 😀
-*/
 
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 // solve
 
 // 1. lay text tu Input
-document.querySelector("textarea").textContent = 
-`underscore_case_duc
- first_name
-Some_Variable 
-  calculate_AGE
-delayed_departure`;
-const text = document.querySelector("textarea").textContent;
+// document.querySelector("textarea").textContent = 
+// `underscore_case_duc
+//  first_name
+//         Some_Variable_dsndnmdsdm
+//   calculate_AGE
+// delayed_departure`;
+
 
 
 const capitalize = (str) => str.replace(str[0], str[0].toUpperCase()); 
@@ -112,6 +149,7 @@ const convert = function(paragraph) {
 
 
 document.querySelector('button').addEventListener("click", function(){
+  const text = document.querySelector("textarea").value;
   const arr_str = convert(text);
   let ans = [];
   let max_length = -1;
@@ -125,7 +163,7 @@ document.querySelector('button').addEventListener("click", function(){
     ans.push(tmp);
   }
   //ouput console
-
+  
   for (let [i, ele] of ans.entries()) {
     const text = ele.padEnd(max_length, " ");
     const mark = "✅".repeat(i + 1);
@@ -134,6 +172,7 @@ document.querySelector('button').addEventListener("click", function(){
   }
 });
 
+*/
 
 
 
